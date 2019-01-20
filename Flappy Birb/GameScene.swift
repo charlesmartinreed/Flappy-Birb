a4b77fe98497dc1ad1377e94b0a851a5d4d1e655
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-20 17:34:30 -0600

    Add birb and background. Add animations for both.

--- Flappy Birb/GameScene.swift
+++ Flappy Birb/GameScene.swift
@@ -11,79 +11,65 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //MARK:- Properties
+    var birb = SKSpriteNode()
+    var levelBG = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        initalizeMainGameScene()
+     
+    }
+    
+    func initalizeMainGameScene() {
+        //add birb
+        let birbTexture = SKTexture(imageNamed: "flappy1")
+        let birbTexture2 = SKTexture(imageNamed: "flappy2")
+        
+        //animate the birb, perpetualy
+        let birbAnimation = SKAction.animate(with: [birbTexture, birbTexture2], timePerFrame: 0.1)
+        let makeBirbFlap = SKAction.repeatForever(birbAnimation)
+        
+        birb = SKSpriteNode(texture: birbTexture)
+        
+        birb.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        birb.zPosition = 1
+        birb.run(makeBirbFlap)
+        
+        addChild(birb)
+        
+        //add the background, animate the background slow to the left
+        let backgroundTexture = SKTexture(imageNamed: "bg")
+        
+        let moveBGOut = SKAction.moveBy(x: -backgroundTexture.size().width, y: 0, duration: 5)
+        let moveBGIn = SKAction.moveBy(x: backgroundTexture.size().width, y: 0, duration: 0)
+        //let moveBGOut = SKAction.moveTo(x: -self.frame.width, duration: 5)
+        //let moveBGIn = SKAction.moveTo(x: self.frame.width, duration: 0)
+        let moveBGSequence = SKAction.sequence([moveBGOut, moveBGIn])
+        
+        //using 3 backgrounds
+        for i in 0...2 {
+            levelBG = SKSpriteNode(texture: backgroundTexture)
+            levelBG.size.height = self.frame.height
+            levelBG.position = CGPoint(x: backgroundTexture.size().width * CGFloat(i), y: self.frame.midY) //first bg is aligned to the left of frame, subsequent ones are aligned to the right of the preceeding bg
+            levelBG.run(SKAction.repeatForever(moveBGSequence))
+            addChild(levelBG)
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        //birb.texture = SKTexture(imageNamed: "flappy2")
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        //birb.texture = SKTexture(imageNamed: "flappy1")
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
     }
 }
